9ffc0fee059314f2e35b9cbe0ee2990e8475b1c3
David Paternina <david@fanxielab.com>
2018-05-29 18:06:10 -0500

    Build docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,9 +3,9 @@ module.exports = {
     description: 'Calendar scheduler component for VueJs',
     ga: "UA-116825927-2",
     head: [
-        ['meta', { property: 'og:url', content: 'https://v-calendar.davidpaternina.com/' }]
-        ['meta', { property: 'og:title', content: 'Vue Calendar Scheduler' }]
-        ['meta', { property: 'og:description', content: 'Calendar scheduler component for VueJs' }]
+        ['meta', { property: 'og:url', content: 'https://v-calendar.davidpaternina.com/' }],
+        ['meta', { property: 'og:title', content: 'Vue Calendar Scheduler' }],
+        ['meta', { property: 'og:description', content: 'Calendar scheduler component for VueJs' }],
         ['meta', { property: 'og:image', content: 'https://v-calendar.davidpaternina.com/assets/fb-jumbo.png' }]
     ],
     themeConfig: {
